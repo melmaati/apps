7fb63f72fb18148ede50f8b874ba85901525503d
Maati <mustaphaabouelmaaty@gmail.com>
2023-02-04 23:42:18 +0200

    commit cond.c for now

---
+++ cond.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+
+
+char x;
+int a;
+printf("guess any number between 1 and 3, then type s");
+scanf("%c", &x);
+if (x == 's'){
+    printf("Is your number = 1? \n");
+    scanf("%c", &x);
+    if (x == 'y'){
+        printf("you see now that i know everyting");
+
+    } else {
+
+    }
+    
+}
+
+
+
+printf("\n");
+return 0;    
+   
+}
